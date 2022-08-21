4c52591c704ecc7abdbee19f068b6d2e74a74828
shaggybelco <98958734+shaggybelco@users.noreply.github.com>
2022-08-21 17:53:45 +0200

    Update db.config.js

--- backend/app/configs/db.config.js
+++ backend/app/configs/db.config.js
@@ -1,6 +1,6 @@
 const client = require('pg').Pool
 
-const DB_URL = 'postgres://odgbyfgxhbrqix:b5b1b41110b190b151dda5f912fcd94391fb9779e6f35c306681d87b049d0239@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d3pecob29d06r4'
+const DB_URL = 'noteDB'
 const pool = new client({connectionString: DB_URL, ssl: {rejectUnauthorized: false}});
 
 module.exports = pool;
